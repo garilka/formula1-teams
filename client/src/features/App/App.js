3dc9c6e4b6b85e382298a495c787d2eb2297de13
Ilka <nemmilka@gmail.com>
2022-09-26 21:13:36 +0200

    feat(client/src/features/App): add setRenderedComponent

--- client/src/features/App/App.js
+++ client/src/features/App/App.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import LoginForm from '../../components/LoginForm/LoginForm';
+import MainPage from '../MainPage/MainPage';
 import './App.css';
 
 const App = () => {
+  const [inMemoryToken] =
+  useState(JSON.parse(localStorage.getItem('token')));
+
+  const [renderedComponent, setRenderedComponent] = useState(<></>);
+
+  useEffect(() => {
+    if (!inMemoryToken) {
+      setRenderedComponent(
+          <LoginForm setRenderedComponent={setRenderedComponent}/>);
+    } else {
+      setRenderedComponent(
+          <MainPage setRenderedComponent={setRenderedComponent}/>);
+    }
+  }, [inMemoryToken]);
+
   return (
     <div className="App">
-      <LoginForm/>
+      {renderedComponent}
     </div>
   );
 };
