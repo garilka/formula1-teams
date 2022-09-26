416bff371e5c2334c1ff211d638af4b83cda1e95
Ilka <nemmilka@gmail.com>
2022-09-26 15:14:50 +0200

    feat(server/src/db/config): add DB_DIALECT env.variable

--- server/src/db/config/config.js
+++ server/src/db/config/config.js
@@ -7,7 +7,7 @@ module.exports = {
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
     port: process.env.DB_PORT,
-    dialect: 'mysql',
+    dialect: process.env.DB_DIALECT,
   },
   test: {
     username: 'root',
