c7f7794e907dd3eb71fba377f8f96afad0bdde29
Ilka <nemmilka@gmail.com>
2022-09-26 16:52:48 +0200

    feat(server/src/db/models): join User with Team

--- server/src/db/models/team.js
+++ server/src/db/models/team.js
@@ -10,7 +10,7 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      // define association here
+      Team.belongsTo(models.User, {foreignKey: 'user_id'});
     }
   }
   Team.init({
