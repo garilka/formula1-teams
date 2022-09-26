cf5ee821b0ea00da2522e5195ab35a1e1f1df781
Ilka <nemmilka@gmail.com>
2022-09-26 19:07:13 +0200

    feat(client/src/features/App): import LoginForm

--- client/src/features/App/App.js
+++ client/src/features/App/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import LoginForm from '../../components/LoginForm/LoginForm';
 import './App.css';
 
 const App = () => {
   return (
     <div className="App">
-      Hello this is client side App!
+      <LoginForm/>
     </div>
   );
 };
