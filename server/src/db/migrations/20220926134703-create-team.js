b6f211377907837b0801fb905941cc1f2cb488ba
Ilka <nemmilka@gmail.com>
2022-09-26 16:44:44 +0200

    feat(server/src/db/): create teams table

---
+++ server/src/db/migrations/20220926134703-create-team.js
@@ -0,0 +1,41 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('teams', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER,
+      },
+      name: {
+        type: Sequelize.STRING,
+      },
+      foundation_year: {
+        type: Sequelize.INTEGER,
+      },
+      wins: {
+        type: Sequelize.INTEGER,
+      },
+      fee_paid: {
+        type: Sequelize.BOOLEAN,
+      },
+      user_id: {
+        type: Sequelize.INTEGER,
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE,
+        defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
+      },
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('teams');
+  },
+};
---
+++ server/src/db/models/team.js
@@ -0,0 +1,27 @@
+'use strict';
+const {
+  Model,
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Team extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Team.init({
+    name: DataTypes.STRING,
+    foundation_year: DataTypes.INTEGER,
+    wins: DataTypes.INTEGER,
+    fee_paid: DataTypes.BOOLEAN,
+    user_id: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Team',
+  });
+  return Team;
+};
