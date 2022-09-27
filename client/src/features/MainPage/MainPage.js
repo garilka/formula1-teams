22bbc5e7c8330c8e67eb12aed67e1b31d176e5ab
Ilka <nemmilka@gmail.com>
2022-09-27 20:57:00 +0200

    feat(client/src/features): create MainPage

--- client/src/features/MainPage/MainPage.js
+++ client/src/features/MainPage/MainPage.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import LogoAndTitle from '../../components/LogoAndTitle/LogoAndTitle';
+import LogoutButton from '../../components/LogoutButton/LogoutButton';
+import LoginButton from '../../components/LoginButton/LoginButton';
 
-const MainPage = () => {
+const MainPage = (props) => {
+  const [renderedButton, setRenderedButton] = useState(<></>);
+  const [inMemoryToken] =
+  useState(JSON.parse(localStorage.getItem('token')));
+
+  useEffect(() => {
+    if (inMemoryToken === null) {
+      setRenderedButton(
+          <LoginButton setRenderedComponent={props.setRenderedComponent}/>,
+      );
+    } else {
+      setRenderedButton(
+          <LogoutButton setRenderedComponent={props.setRenderedComponent}/>,
+      );
+    }
+  }, [inMemoryToken]);
+
+
   return (
     <div>
-      Hello, you succesfully logged in!
+      <LogoAndTitle />
+      Hello, this is main page!
+      <br/>
+      {renderedButton}
     </div>
   );
 };
