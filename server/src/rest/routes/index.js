358787173eec8543fe13e6a2c7af3b77f4fefe5b
Ilka <nemmilka@gmail.com>
2022-09-27 14:10:23 +0200

    feat(server/src/rest/routes/index.js): import user & api routes

--- server/src/rest/routes/index.js
+++ server/src/rest/routes/index.js
@@ -1,7 +1,11 @@
 import express from 'express';
+import apiRouter from './apiRouter.js';
 import baseRouter from './baseRouter.js';
+import userRouter from './userRouter.js';
 const router = express.Router();
 
 router.use('/', baseRouter);
+router.use('/api', apiRouter);
+router.use('/api/user', userRouter);
 
-export default baseRouter;
+export default router;
