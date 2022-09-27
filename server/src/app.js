91cf63b277bdff5b3226c6494f4bf6718c8a5b43
Ilka <nemmilka@gmail.com>
2022-09-27 14:11:06 +0200

    feat(server/src/app.js): import auth middleware

--- server/src/app.js
+++ server/src/app.js
@@ -2,11 +2,13 @@ import dotenv from 'dotenv';
 import express from 'express';
 import cors from 'cors';
 import routes from './rest/routes/index.js';
+import auth from './rest/middleware/auth.js';
 dotenv.config();
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.use(auth);
 app.use(routes);
 
 export default app;
