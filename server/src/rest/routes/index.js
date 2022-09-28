f67049084c39d31354adfcd1d0428ca73a1e186f
Ilka <nemmilka@gmail.com>
2022-09-28 16:40:57 +0200

    feat(server/src/rest/routes/index.js): import teamRouter

--- server/src/rest/routes/index.js
+++ server/src/rest/routes/index.js
@@ -2,10 +2,12 @@ import express from 'express';
 import apiRouter from './apiRouter.js';
 import baseRouter from './baseRouter.js';
 import userRouter from './userRouter.js';
+import teamRouter from './teamRouter.js';
 const router = express.Router();
 
 router.use('/', baseRouter);
 router.use('/api', apiRouter);
 router.use('/api/user', userRouter);
+router.use('/api/team', teamRouter);
 
 export default router;
