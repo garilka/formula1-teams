66e35096f328eb02a78c335c760b97dd6318b6dd
Ilka <nemmilka@gmail.com>
2022-09-28 23:30:12 +0200

    feat(client/src/hooks): create usePostList

---
+++ client/src/hooks/usePostList.js
@@ -0,0 +1,31 @@
+import {useState} from 'react';
+
+const usePostList = () => {
+  const [postList, setPostList] = useState([]);
+
+  const handleFetch = async () => {
+    fetch(process.env.REACT_APP_BACKEND_URL + '/api/team', {
+      method: 'get',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+      },
+    }).then(async (response) => {
+      if (response.status === 200) {
+        setPostList(await response.json());
+      }
+      if (response.status === 500) {
+        throw new Error('Server error');
+      }
+    }).catch((error) => {
+      throw new Error(error);
+    });
+  };
+
+  return {
+    handleFetch,
+    postList,
+  };
+};
+
+export default usePostList;
