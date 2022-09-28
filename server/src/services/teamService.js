db9c533bb7e47b9b8894b670a3d769afa33048cb
Ilka <nemmilka@gmail.com>
2022-09-28 23:22:18 +0200

    fix(server/src/services/teamService): update column list

--- server/src/services/teamService.js
+++ server/src/services/teamService.js
@@ -2,7 +2,9 @@ import Model from '../db/models';
 const {Team} = Model;
 
 const getAllTeams = async () => {
-  const teams = await Team.findAll();
+  const teams = await Team.findAll({
+    attributes: ['name', 'foundation_year', 'wins', 'fee_paid'],
+  });
   return teams;
 };
 
