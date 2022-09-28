cd89441da7557db165c22ec1fdfe8f48a2cc45d8
Ilka <nemmilka@gmail.com>
2022-09-28 23:29:07 +0200

    feat(client/src/components): create PostList

---
+++ client/src/components/PostList/PostList.js
@@ -0,0 +1,44 @@
+import React, {useEffect, useRef} from 'react';
+import BlogPost from '../BlogPost/BlogPost';
+import usePostList from '../../hooks/usePostList';
+
+const PostList = (props) => {
+  const {handleFetch, postList} = usePostList();
+  const mounted = useRef(false);
+  let listItems = [];
+
+  useEffect(() => {
+    mounted.current = true;
+    handleFetch();
+    return () => mounted.current = false;
+  }, []);
+
+  if (postList !== []) {
+    listItems = postList.map((post) => {
+      const feePaidIcon = post.fee_paid === true? '✔':'✘';
+      return (
+        <BlogPost
+          key={post.name}
+          teamName={post.name}
+          worldChampionships={post.wins}
+          yearOfFoundation={post.foundation_year}
+          feePaid={feePaidIcon}
+        />
+      );
+    });
+  }
+
+  if (postList !== [] && postList !== null && postList !== undefined) {
+    return (
+      <div>
+        {listItems}
+      </div>
+    );
+  } else {
+    return (
+      <div className='noTeam'>Team list is empty yet...</div>
+    );
+  }
+};
+
+export default PostList;
