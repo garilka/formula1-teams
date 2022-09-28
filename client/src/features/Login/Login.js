93716de426a921b8667743eb502287a0557d44c0
Ilka <nemmilka@gmail.com>
2022-09-28 14:16:19 +0200

    style(client/src/features/Login): add CSS

--- client/src/features/Login/Login.js
+++ client/src/features/Login/Login.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import './Login.css';
 import LogoAndTitle from '../../components/LogoAndTitle/LogoAndTitle';
 import LoginForm from '../../components/LoginForm/LoginForm';
 
 const Login = (props) => {
   return (
-    <div>
+    <div className='loginWrapper'>
       <LogoAndTitle />
       <LoginForm setRenderedComponent={props.setRenderedComponent}/>
     </div>
