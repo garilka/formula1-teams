7d065572d0cecb6abd46ef457dd1f11024076634
Ilka <nemmilka@gmail.com>
2022-09-28 16:38:33 +0200

    fix(server/src/rest/controllers/userController.js): res.status

--- server/src/rest/controllers/userController.js
+++ server/src/rest/controllers/userController.js
@@ -3,7 +3,7 @@ import {ValidationError} from '../../services/errorService.js';
 
 const post = async (req, res) => {
   try {
-    return res.json(await userService.createUser({...req.body}));
+    return res.status(200).json(await userService.createUser({...req.body}));
   } catch (error) {
     if (error instanceof ValidationError) {
       return res.status(400).json({message: error.message});
