f291b636e2eeb2a2f3ec7caa33202179570440f7
Ilka <nemmilka@gmail.com>
2022-09-28 16:39:57 +0200

    feat(server/src/rest/services): create teamService

---
+++ server/src/services/teamService.js
@@ -0,0 +1,9 @@
+import Model from '../db/models';
+const {Team} = Model;
+
+const getAllTeams = async () => {
+  const teams = await Team.findAll();
+  return teams;
+};
+
+export default {getAllTeams};
