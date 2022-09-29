c75dc9b91b15128ff8eca990e6c100deec287988
Ilka <nemmilka@gmail.com>
2022-09-29 20:13:35 +0200

    feat(server/src/rest/controllers/teamController.js): add patch()

--- server/src/rest/controllers/teamController.js
+++ server/src/rest/controllers/teamController.js
@@ -32,8 +32,20 @@ const post = async (req, res) => {
 
 const patch = async (req, res) => {
   try {
+    const reqBodyKeywords = /name|wins|foundationYear|feePaid/g;
+    if (req.body.oldName &&
+      Object.keys(req.body).some((key) => reqBodyKeywords.test(key))) {
+      await teamService.updateTeam(req.body);
+      return res.status(200).json(await teamService.updateTeam({...req.body}));
+    }
   } catch (error) {
-
+    if (error instanceof ValidationError) {
+      return res.status(400).json({message: error.message});
+    }
+    if (error instanceof JsonWebTokenError) {
+      return res.status(401).json({message: error.message});
+    }
+    return res.status(500).json({message: error.message});
   }
 };
 
