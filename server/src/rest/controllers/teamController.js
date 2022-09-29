aee5532525927a8780c77fecf20a3a619a13a456
Ilka <nemmilka@gmail.com>
2022-09-29 17:40:22 +0200

    feat(services/src/rest/controllers/teamController.js): import decodeToken

--- server/src/rest/controllers/teamController.js
+++ server/src/rest/controllers/teamController.js
@@ -1,6 +1,9 @@
 import teamService from '../../services/teamService';
 import {ValidationError} from '../../services/errorService';
-// import jwt from 'jsonwebtoken';
+import encryptionService from '../../services/encryptionService';
+import jwt from 'jsonwebtoken';
+
+const {JsonWebTokenError} = jwt;
 
 const get = async (req, res) => {
   try {
@@ -12,15 +15,16 @@ const get = async (req, res) => {
 
 const post = async (req, res) => {
   try {
-    const decodedToken = teamService.decodeToken(req.headers['authorization']);
+    const decodedToken =
+      encryptionService.decodeToken(req.headers['authorization']);
     req.body.userId = decodedToken.payload.id;
     return res.status(200).json(await teamService.createTeam({...req.body}));
   } catch (error) {
     if (error instanceof ValidationError) {
       return res.status(400).json({message: error.message});
     }
-    if (error instanceof token) {
-      return res.status(400).json({message: error.message});
+    if (error instanceof JsonWebTokenError) {
+      return res.status(401).json({message: error.message});
     }
     return res.status(500).json({message: error.message});
   }
