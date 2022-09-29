9b3af7d6d798f6529185ed393e20d99a73aba073
Ilka <nemmilka@gmail.com>
2022-09-29 20:37:57 +0200

    feat(server/src/rest/routes/teamRouter.js): add delete route

--- server/src/rest/controllers/index.js
+++ server/src/rest/controllers/index.js
@@ -1,5 +1,6 @@
 import baseController from './baseController.js';
 import userController from './userController.js';
 import authController from './authController.js';
+import teamController from './teamController.js';
 
-export {baseController, userController, authController};
+export {baseController, userController, authController, teamController};
--- server/src/rest/routes/teamRouter.js
+++ server/src/rest/routes/teamRouter.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import teamController from '../controllers/teamController';
+import {teamController} from '../controllers/index.js';
 const teamRouter = express.Router();
 
 teamRouter.get('/all', teamController.get);
 teamRouter.post('/', teamController.post);
 teamRouter.patch('/', teamController.patch);
+teamRouter.delete('/', teamController.del);
 
 export default teamRouter;
