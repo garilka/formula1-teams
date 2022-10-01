c4d2a41e2d7e0db158216951e0e56957ace83c8b
Ilka <nemmilka@gmail.com>
2022-10-01 19:38:25 +0200

    style(client/src/features/CreateTeamPage): add CSS

--- client/src/features/CreateTeamPage/CreateTeamPage.js
+++ client/src/features/CreateTeamPage/CreateTeamPage.js
@@ -10,70 +10,73 @@ const CreateTeamPage = (props) => {
   );
 
   return (
-    <form
-      className='CreateTeamForm'
-      onSubmit={handleSubmit}>
-      <div className='topIcons'>
-        <div className='carIcon'>Create new team🏎️</div>
-        <div
-          className='gearIcon'
-          onClick={handleExit}>
-          ✖️
+    <div className='CreateTeam'>
+      <form
+        className='CreateTeamForm'
+        onSubmit={handleSubmit}>
+        <div className='topIcons'>
+          <div className='carIcon'>Create new team ✨</div>
+          <div
+            className='gearIcon'
+            onClick={handleExit}>
+            ✖️
+          </div>
         </div>
-      </div>
 
-      <div className='teamName'>
-        <label className='infoLabel'>Team name</label>
-        <br/>
-        <input
-          name='name'
-          id='name'
-          type='text'
-          value={values.name}
-          onChange={handleChange}
-        />
-      </div>
-
-      <div className='teamInfos'>
-        <div className='worldChampionships'>
-          <label className='infoLabel'>World Championships</label>
+        <div className='teamName'>
+          <label className='infoLabel'>Team name</label>
           <br/>
           <input
-            name='wins'
-            id='wins'
+            name='name'
+            id='name'
             type='text'
-            value={values.wins}
+            placeholder='Ex. Hot Wheels'
+            value={values.name}
             onChange={handleChange}
           />
         </div>
-        <div className='yearOfFoundation'>
-          <label className='infoLabel'>Year Of Foundation</label>
-          <br/>
-          <input
-            name='foundationYear'
-            id='foundationYear'
-            type='number'
-            min='1946'
-            max='2023'
-            value={values.foundationYear}
-            onChange={handleChange}
-          />
+
+        <div className='teamInfos'>
+          <div className='worldChampionships'>
+            <label className='infoLabel'>World Championships</label>
+            <br/>
+            <input
+              name='wins'
+              id='wins'
+              type='text'
+              value={values.wins}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='yearOfFoundation'>
+            <label className='infoLabel'>Year Of Foundation</label>
+            <br/>
+            <input
+              name='foundationYear'
+              id='foundationYear'
+              type='number'
+              min='1946'
+              max='2023'
+              value={values.foundationYear}
+              onChange={handleChange}
+            />
+          </div>
+          <div className='feePaid'>
+            <label className='infoLabel'>Entry Fee Paid</label>
+            <br/>
+            <input
+              name='feePaid'
+              id='feePaid'
+              type='checkbox'
+              value={values.feePaid}
+              onClick={handleChecked}
+            />
+          </div>
         </div>
-        <div className='feePaid'>
-          <label className='infoLabel'>Entry Fee Paid</label>
-          <br/>
-          <input
-            name='feePaid'
-            id='feePaid'
-            type='checkbox'
-            value={values.feePaid}
-            onClick={handleChecked}
-          />
-        </div>
-      </div>
-      <button>Create</button>
-      {responseError.message ? <p>{responseError.message}</p> : null}
-    </form>
+        {responseError.message ? <p>{responseError.message}</p> : null}
+        <button>Create</button>
+      </form>
+    </div>
   );
 };
 
